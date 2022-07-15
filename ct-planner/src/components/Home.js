f7cf70aa1be597a8148c82c6416ba5177bcebe88
ckntucker <ckntucker@gmail.com>
2022-07-15 11:42:16 -1000

    basic setup, deleted some install stuff

---
+++ ct-planner/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Routes, Route, Link, Navigate } from 'react-router-dom'
+import Planner from './Planner';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Component</h1>
+        </div>
+    );
+};
+
+export default Home;
